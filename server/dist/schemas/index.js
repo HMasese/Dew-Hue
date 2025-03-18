baa4f7277326a2ce37907e84e93e585e5c9050d6
Harriet M <harrietm616@gmail.com>
2025-03-18 15:43:55 -0400

    deleted file

--- server/dist/schemas/index.js
+++
@@ -1,7 +0,0 @@
-import typeDefs from './typeDefs.js';
-<<<<<<< HEAD
-import resolvers from './resolvers';
-=======
-import resolvers from './resolvers.js';
->>>>>>> 78d455a755d7ed000388f8b7d448e80d6d7d61dc
-export { typeDefs, resolvers };
